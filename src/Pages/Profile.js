76732bda15aafc1e285a58d7e1cf4f4869bffe5d
Srujan <srujan.deshpande@gmail.com>
2020-08-10 23:47:57 +0530

    Links table

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -10,8 +10,24 @@ export default function Profile(props) {
   let { id } = useParams();
 
   return (
-    <div>
-      Viewing Links for: {id}
+    <div className="App">
+      <div className="App-header">
+        <h1>{id}'s Links!</h1>
+        <table>
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="https://google.com">https://google.com</a></td>
+            <td>Mock</td>
+          </tr>
+        </tbody>
+        </table>
+      </div>
     </div>
   );
 }
