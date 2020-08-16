93af30d6d9ecbe5d1227bf62c74ca3d92cf613ea
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-08-16 22:59:45 +0530

    Button

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -13,6 +13,13 @@ export default function Profile(props) {
     <div className="App">
       <div className="App-header">
         <h1>{id}'s Links!</h1>
+        <ul>
+          <li>
+            <a href="https://google.com">
+              <button type="button" className="btn btn-info">Element</button>
+            </a>
+          </li>
+        </ul>
         <table>
         <thead>
           <tr>
